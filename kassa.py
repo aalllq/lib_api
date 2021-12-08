7fcf17887ed7dcfff44b8ea9e11b07ae93bc1964
aalllq <34611623+aalllq@users.noreply.github.com>
2021-12-08 16:57:08 +0300

    repa

--- kassa.py
+++ kassa.py
@@ -12,9 +12,15 @@ import glob
 from lib.get_token import get_token
 
 
-#mode=(int(input('Выберите режим работы \n 1=Работа с файлом \n 2=Работа с БД \n')))
-tok=get_token()
+mode=(int(input('Выберите режим работы \n 1=Работа с файлом \n 2=Работа с БД \n, 3=Работа без API \n')))
+
+print(get_token())
+'''
 if tok[0]== False:
-    input("authFalse, press ant key to exit \n" +str(tok))
-    exit()
-#print(env_vars)
+    while True:
+        f_mode=int((input("authFalse, press 1 = continue \n 2 = exit" +str(tok)))
+        if f_mode != 1 or f_mode != 2
+            print(env_vars)
+        else:break'''
+
+input('exit')
--- lib/get_token.py
+++ lib/get_token.py
@@ -8,20 +8,24 @@ from  requests.utils import quote
 urllib3.disable_warnings()
 env_vars = {}  
 #f =open(os.path.join(os.path.split(os.path.dirname(__file__))[0], '.env'))
-f =open(glob.glob('.env')[0],'r')
+f =open(glob.glob('data/.env')[0],'r')
+
 for line in f:
-    if line.startswith('#') or not line.strip():
-        continue
+    if line.startswith('#') or not line.strip():continue
     key, value = line.strip().split('=', 1)
     env_vars[key]=value
 
+
 def get_token():
-    gettok = requests.post(env_vars["url"]+'/api/v1/token',
-    headers = {'Authorization' : 'Basic Og==', 'Content-type':'application/x-www-form-urlencoded',
-    'Accept': 'application/json, text/plain, */*'}, verify=False,
-    data = 'grant_type=password&username='+quote(env_vars['userapi'])+'&password='+quote(env_vars['passapi]'))
-    if gettok.status_code == 200:
-        tok=gettok.json()['access_token']
-        return(True,tok)
+    if not "apiuser" in env_vars or not "apipass" in env_vars:
+        return(False,"not apipass or apiuser in .env")
     else:
-        return(False,gettok.text)
+        try:
+            gettok = requests.post(env_vars["url"]+'/api/v1/token',
+            headers = {'Authorization' : 'Basic Og==', 'Content-type':'application/x-www-form-urlencoded',
+            'Accept': 'application/json, text/plain, */*'}, verify=False,
+            data = 'grant_type=password&username=' + quote(env_vars['apiuser']) + '&password=' + quote(env_vars['apipass']))
+            return gettok.json()['access_token'] if gettok.status_code == 200 else False,gettok.text
+        except requests.exceptions.RequestException as err:
+            return "OOps:",err
+            
